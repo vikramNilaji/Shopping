8345ab9f18fb6596e5403b776cc770cdf3d2ab4e
Vikram Nilaji <vik.nilaji@gmail.com>
2026-07-20 20:32:58 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js'; 
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 // Import the combined auth routes file (Note the explicit .js extension)
-import authRoutes from './routes/auth.js';
+import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
@@ -11,11 +11,11 @@ const app = express();
 // Middleware to parse JSON body data
 app.use(express.json());
 
-// connectDB(); 
+// connectDB();
 
 // Link your auth routes file to the base path
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-connectDB()
+connectDB();
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
